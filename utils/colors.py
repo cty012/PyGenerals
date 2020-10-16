a5d36f86a016a081c62fe2bdaebb17ef97747356
DanielC <56935742+cty012@users.noreply.github.com>
2020-10-16 11:27:10 +0800

    Fix color (magenta)

--- utils/colors.py
+++ utils/colors.py
@@ -17,7 +17,7 @@ p_blue = rgb(0, 0, 255)
 p_green = rgb(0, 128, 0)
 p_yellow = rgb(200, 150, 0)
 p_cyan = rgb(0, 160, 160)
-p_magenta = rgb(180, 35, 260)
+p_magenta = rgb(180, 35, 255)
 p_brown = rgb(128, 80, 0)
 p_lavender = rgb(190, 160, 220)
 
